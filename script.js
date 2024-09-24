4696c581022d7e8af0ac1624796ce2f144d2cb75
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-09-24 08:35:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const sizeInfo = document.querySelectorAll('sizeInfo');
+const sizeInfo = document.getElementById('sizeInfo');
 
 function updateSize() {
 	let width = window.innerWidth;
