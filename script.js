dd843610dcceeb8682d9e36613eada8d119db249
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-09-24 08:31:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const sizeInfo = document.querySelectorAll('sizeInfo');
+
+function updateSize() {
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	sizeInfo.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+	
+}
+window.addEventListener('resize', updateSize);
+updateSize();
